67c84ac7e2c7a5fa5eb0251041a210594cb72b8e
Christian Mayer <thefox21at@gmail.com>
2013-08-13 00:12:59 +0200

    Base post.

--- import.php
+++ import.php
@@ -28,13 +28,47 @@ if(
 	|| !isset($paramters['tumblr']['consumer_key'])
 	|| !isset($paramters['tumblr']['consumer_secret'])
 	|| !isset($paramters['tumblr']['token'])
-	|| !isset($paramters['tumblr']['secret'])
+	|| !isset($paramters['tumblr']['token_secret'])
 ){
 	print "ERROR: parameters invalid.\n";
 	var_export($paramters); print "\n";
 	exit(1);
 }
 
-if($paramters['tumblr']['consumer_key'] == 'w' && $paramters['tumblr']['consumer_secret'] == 'x' && $paramters['tumblr']['token'] == 'y' && $paramters['tumblr']['secret'] == 'z'){
+if($paramters['tumblr']['consumer_key'] == 'w' && $paramters['tumblr']['consumer_secret'] == 'x' && $paramters['tumblr']['token'] == 'y' && $paramters['tumblr']['token_secret'] == 'z'){
 	
 }
+
+$client = new Tumblr\API\Client($paramters['tumblr']['consumer_key'], $paramters['tumblr']['consumer_secret'], $paramters['tumblr']['token'], $paramters['tumblr']['token_secret']);
+
+if(!isset($paramters['tumblr']['blog'])){
+	print "You havn't set up a blog name.\nAvailable names are:\n";
+	foreach($client->getUserInfo()->user->blogs as $blog){
+		print "\t".$blog->name."\n";
+	}
+	exit(1);
+}
+
+#var_export($client);exit();
+
+
+$data = array(
+	#'api_key' => $paramters['tumblr']['consumer_key'],
+	#'type' => 'quote',
+	'type' => 'text',
+	#'base-hostname' => $paramters['tumblr']['blog'],
+	#'base-hostname' => $paramters['tumblr']['blog'].'.tumblr.com',
+	'text' => 'Hi from API.',
+	#'source' => 'source1',
+);
+try{
+	$res = $client->createPost($paramters['tumblr']['blog'].'.tumblr.com', $data);
+	#$res = $client->createPost($paramters['tumblr']['blog'], $data);
+	var_export($res);
+}
+catch(Exception $e){
+	#var_export($e);
+	print "ERROR: ".$e->getMessage()."\n";
+	exit(1);
+}
+
