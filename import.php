f6c07779c02b4b1107dc8ea254e76286fe3c38bb
Christian Mayer <thefox21at@gmail.com>
2013-08-16 14:47:41 +0200

    Import.

--- import.php
+++ import.php
@@ -38,9 +38,6 @@ if(
 	exit(1);
 }
 
-if($paramters['tumblr']['consumer_key'] == 'w' && $paramters['tumblr']['consumer_secret'] == 'x' && $paramters['tumblr']['token'] == 'y' && $paramters['tumblr']['token_secret'] == 'z'){
-	
-}
 
 $client = new Tumblr\API\Client($paramters['tumblr']['consumer_key'], $paramters['tumblr']['consumer_secret'], $paramters['tumblr']['token'], $paramters['tumblr']['token_secret']);
 
@@ -52,20 +49,109 @@ if(!isset($paramters['tumblr']['blog'])){
 	exit(1);
 }
 
-#var_export($client);exit();
+
+$options = array(
+	'type' => 'quote',
+	#'quote' => 'Text \'y\' "x" '.date('Y/m/d H:i:s'),
+	#'source' => 'Source',
+	#'tags' => 'Test',
+	'state' => 'queue',
+);
 
 
+$lines = 0;
+$successfull = 0;
+$errors = 0;
 
 
-$options = array('type' => 'quote', 'quote' => 'Text '.date('Y/m/d H:i:s'), 'source' => 'Source', 'tags' => 'Test', 'state' => 'queue');
-
-
-try{
-	$res = $client->createPost($paramters['tumblr']['blog'], $options);
+$fh = fopen('posts.txt', 'r');
+while(!feof($fh)){
+	
+	$lines++;
+	
+	$row = fgets($fh);
+	$row = str_replace("\n", '', $row);
+	$row = str_replace("\r", '', $row);
+	
+	#print "row '".$row."' '$text' \n";
+	
+	
+	$text = $row;
+	$source = '';
+	$tags = '';
+	
+	$pos = strrpos($row, ' - ');
+	if($pos !== false){
+		$text = substr($row, 0, $pos);
+		$source = substr($row, $pos + 3);
+		
+		#print "\t source pos: $pos\n";
+		#print "\t substr '$source'\n";
+		
+		$pos = strpos($source, '#');
+		if($pos !== false){
+			
+			$tags = substr($source, $pos + 1);
+			$source = substr($source, 0, $pos);
+			
+			
+			#print "\t tag pos: $pos '$source' '$tags'\n";
+		}
+		
+	}
+	else{
+		$pos = strpos($row, '#');
+		if($pos !== false){
+			$text = substr($row, 0, $pos);
+			$tags = substr($row, $pos + 1);
+		}
+	}
+	
+	$text = trim($text);
+	$source = trim($source);
+	$tags = trim($tags);
+	
+	if($text){
+		$options['quote'] = $text;
+		if($source){
+			$options['source'] = $source;
+		}
+		if($tags){
+			$options['tags'] = $tags;
+		}
+		
+		
+		print "post '$text' | '$source' | '$tags' ... ";
+		
+		try{
+			$res = false;
+			$res = $client->createPost($paramters['tumblr']['blog'], $options);
+			print 'done: ';
+			if($res){
+				$successfull++;
+				print $res->id;
+			}
+			else{
+				$errors++;
+				print 'failed';
+			}
+		}
+		catch(Exception $e){
+			print 'ERROR: '.$e->getMessage();
+			$errors++;
+			#exit(1);
+		}
+		print "\n";
+	}
+	
+	#sleep(1);
+	
+	if($exit) break;
 }
-catch(Exception $e){
-	#var_export($e);
-	print "ERROR: ".$e->getMessage()."\n";
-	exit(1);
-}
+fclose($fh);
+
+print "\nlines: ".$lines."\nsuccessfull: ".$successfull."\nerrors: ".$errors."\n";
+
+exit($errors);
+
 
